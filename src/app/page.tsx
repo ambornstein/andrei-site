3f1b89d7e1ce637b0fe92a8e4e79d12e64eeaad1
Andrei <amborn02@gmail.com>
2024-12-26 22:08:07 -0600

    give me the images please

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
       <main className="flex flex-row gap-4 overflow-x-auto scroll-smooth snap-x snap-mandatory *:snap-center *:scroll-mt-10">
         <div id="about" className="min-w-full h-full items-center">
           <div className="w-fit flow-root m-auto ">
-            <img src="/public/andrei-crop.jpg" className="float-start me-4 w-5/12"></img>
+            <img src="public/andrei-crop.jpg" className="float-start me-4 w-5/12"></img>
             <p className="inline text-lg">My name is Andrei and I am a</p>
               <ul className="inline w-fit">
                 <li className="m-auto bg-neutral-300 p-1 my-2 rounded-sm">Fullstack Developer</li>
@@ -41,28 +41,28 @@ export default function Home() {
         <div id="sites" className="min-w-full items-center flex flex-col sm:gap-8 gap-2
           *:border-fuchsia-950 *:border-4 *:rounded-md *:p-2">
           <div className="hover:bg-neutral-300">
-            <Image src="/public/aquarium-landing.PNG"
+            <Image src="public/aquarium-landing.PNG"
               alt="Next.js logo"
               width={600}
               height={300}
               priority />
           </div>
           <div className="hover:bg-neutral-300">
-            <Image src="/public/grey-matter-landing.PNG"
+            <Image src="public/grey-matter-landing.PNG"
               alt="Next.js logo"
               width={600}
               height={300}
               priority />
           </div>
           <div className="hover:bg-neutral-300">
-            <Image src="/public/kevin-site-landing.PNG"
+            <Image src="public/kevin-site-landing.PNG"
               alt="Next.js logo"
               width={600}
               height={300}
               priority />
           </div>
           <div className="hover:bg-neutral-300">
-            <Image src="/public/dashboard_exp.PNG"
+            <Image src="public/dashboard_exp.PNG"
               alt="Next.js logo"
               width={600}
               height={300}
@@ -72,7 +72,7 @@ export default function Home() {
         <div id="games" className="min-w-full items-center flex flex-col gap-8">
           <Image
             className="dark:invert"
-            src="/public/next.svg"
+            src="public/next.svg"
             alt="Next.js logo"
             width={180}
             height={38}
