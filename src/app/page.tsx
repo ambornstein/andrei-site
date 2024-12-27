fc3b48d4647f494a763983e5455103c6371a5c8f
Andrei <amborn02@gmail.com>
2024-12-26 22:00:27 -0600

    first deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,70 +5,84 @@ export default function Home() {
   const scrollTo = (id: string) => {
     const element = document.getElementById(id);
     element?.scrollIntoView({
-      behavior: "smooth"
+      behavior: "smooth",
+      block: "nearest",
     })
+    window.scrollBy(0,0)
   }
 
   return (
-    <>
-      <header className="fixed w-fit top-4 left-1/2 sm:transform -translate-x-1/2">
-        <div className="w-fit m-auto bg-neutral-300 ring-4 p-2  rounded-md">
-          <h1 className="text-4xl w-full m-auto">Andrei Bornstein</h1>
+    <div className="dark:bg-neutral-800 min-h-screen p-4 gap-16 font-[family-name:var(--font-geist-sans)]">
+      <header className="m-auto">
+        <div className="w-fit m-auto bg-stone-300 ring-2 p-2 rounded-md ring-neutral-700">
+          <h1 className="text-4xl">Andrei Bornstein</h1>
         </div>
-        <ul className="bg-slate-500 rounded-2xl p-4 flex m-auto my-4 flex-row  gap-4 text-nowrap
-        *:text-lg *:p-2 *:rounded-full">
-          <a className="focus:bg-zinc-400" tabIndex={0} onClick={() => scrollTo("sites")}>Websites</a>
-          <a className="focus:bg-zinc-400" tabIndex={1} onClick={() => scrollTo("games")}>Games</a>
-          <a className="focus:bg-zinc-400" tabIndex={2} onClick={() => scrollTo("games")}>2D/3D Art</a>
+        <ul className="flex flex-row justify-space w-fit max-w-full bg-neutral-400 rounded-2xl p-2 m-auto my-4 text-nowrap
+        *:text-md *:p-1 *:rounded-full">
+          <a className="focus:bg-stone-500 hover:text-gray-300" tabIndex={0} onClick={() => scrollTo("about")}>About</a>
+          <a className="focus:bg-stone-500 hover:text-gray-300" tabIndex={0} onClick={() => scrollTo("sites")}>Websites</a>
+          <a className="focus:bg-stone-500 hover:text-gray-300" tabIndex={1} onClick={() => scrollTo("games")}>Games</a>
+          <a className="focus:bg-stone-500 hover:text-gray-300" tabIndex={2} onClick={() => scrollTo("games")}>2D/3D Art</a>
         </ul>
       </header>
-      <div className="dark:bg-neutral-800 items-center justify-between overflow-auto min-h-screen p-4 gap-16 sm:p-0 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-row overflow-auto scroll-smooth snap-x snap-mandatory pb-8 items-center sm:items-start *:pt-44">
-          <div id="sites" className="snap-center min-w-full items-center flex flex-col sm:gap-8 gap-2
-          *:border-violet-400 *:border-2 *:rounded-md *:p-2">
-            <div className="hover:bg-neutral-300">
-              <Image src="/aquarium-landing.PNG"
-                alt="Next.js logo"
-                width={600}
-                height={300}
-                priority />
-            </div>
-            <div className="">
-              <Image src="/grey-matter-landing.PNG"
-                alt="Next.js logo"
-                width={600}
-                height={300}
-                priority />
-            </div>
-            <div className="">
-              <Image src="/kevin-site-landing.PNG"
-                alt="Next.js logo"
-                width={600}
-                height={300}
-                priority />
-            </div>
-            <div className="">
-              <Image src="/dashboard_exp.PNG"
-                alt="Next.js logo"
-                width={600}
-                height={300}
-                priority />
-            </div>
+      <main className="flex flex-row gap-4 overflow-x-auto scroll-smooth snap-x snap-mandatory *:snap-center *:scroll-mt-10">
+        <div id="about" className="min-w-full h-full items-center">
+          <div className="w-fit flow-root m-auto ">
+            <img src="/public/andrei-crop.jpg" className="float-start me-4 w-5/12"></img>
+            <p className="inline text-lg">My name is Andrei and I am a</p>
+              <ul className="inline w-fit">
+                <li className="m-auto bg-neutral-300 p-1 my-2 rounded-sm">Fullstack Developer</li>
+                <li className="m-auto bg-neutral-300 p-1 my-2 rounded-sm">Game Dev</li>
+                <li className="m-auto bg-neutral-300 p-1 my-2 rounded-sm">Digital & 3D Artist</li>
+              </ul>
+            
           </div>
-          <div id="games" className="snap-center min-w-full items-center flex flex-col gap-8">
-            <Image
-              className="snap-center dark:invert"
-              src="/next.svg"
+        </div>
+        <div id="sites" className="min-w-full items-center flex flex-col sm:gap-8 gap-2
+          *:border-fuchsia-950 *:border-4 *:rounded-md *:p-2">
+          <div className="hover:bg-neutral-300">
+            <Image src="/public/aquarium-landing.PNG"
               alt="Next.js logo"
-              width={180}
-              height={38}
-              priority
-            />
+              width={600}
+              height={300}
+              priority />
           </div>
-        </main>
-        <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        </footer>
-      </div>
-    </>
+          <div className="hover:bg-neutral-300">
+            <Image src="/public/grey-matter-landing.PNG"
+              alt="Next.js logo"
+              width={600}
+              height={300}
+              priority />
+          </div>
+          <div className="hover:bg-neutral-300">
+            <Image src="/public/kevin-site-landing.PNG"
+              alt="Next.js logo"
+              width={600}
+              height={300}
+              priority />
+          </div>
+          <div className="hover:bg-neutral-300">
+            <Image src="/public/dashboard_exp.PNG"
+              alt="Next.js logo"
+              width={600}
+              height={300}
+              priority />
+          </div>
+        </div>
+        <div id="games" className="min-w-full items-center flex flex-col gap-8">
+          <Image
+            className="dark:invert"
+            src="/public/next.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+          />
+        </div>
+      </main>
+      <footer className="flex flex-wrap items-center justify-center">
+        Operation Asymmetry
+      </footer>
+    </div>
   );
 }
